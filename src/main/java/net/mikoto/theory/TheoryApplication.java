cf31100e8ced88798aaf88f79796272c6ab840ca
mikoto <mikoto@ungine.cn>
2023-12-18 22:30:54 +0800

    init

---
+++ src/main/java/net/mikoto/theory/TheoryApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.theory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mikoto/theory/TheoryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.theory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
